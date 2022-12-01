1ab6844416d8b292975f5fd567ab19ad78e9335b
Tapasvi Patel <patapasv@sheridancollege.ca>
2022-12-01 01:34:58 -0500

    Image added to the Specialist card view. Toast message added when the card is selected.

--- app/src/main/java/project/st991591950/dhruvparthtapasvi/specialist/MyRecycleView.kt
+++ app/src/main/java/project/st991591950/dhruvparthtapasvi/specialist/MyRecycleView.kt
@@ -3,7 +3,10 @@ package project.st991591950.dhruvparthtapasvi.specialist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.specialist_item.view.*
 import project.st991591950.dhruvparthtapasvi.R
@@ -11,9 +14,11 @@ import project.st991591950.dhruvparthtapasvi.R
 class MyRecycleView(private val sampleList: List<SpecialistList>) : RecyclerView.Adapter <MyRecycleView.MyViewHolder>() {
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.imageView
         val specialistNameView: TextView = itemView.textView_specialistName
         val specialityView: TextView = itemView.textView_speciality
         val clinicNameView: TextView = itemView.textView_clinicName
+        val specialistcardView:RelativeLayout = itemView.specialistCard
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -25,10 +30,15 @@ class MyRecycleView(private val sampleList: List<SpecialistList>) : RecyclerView
 
         val currentItem = sampleList[position]
 
+        holder.imageView.setImageResource(currentItem.imageResource)
         holder.specialistNameView.text = currentItem.specialistName
         holder.specialityView.text = currentItem.speciality
         holder.clinicNameView.text = currentItem.clinicName
 
+        holder.specialistcardView.setOnClickListener{
+            Toast.makeText(holder.specialistcardView.context,
+                "You selected " +currentItem.specialistName+ ".", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount() = sampleList.size
--- app/src/main/java/project/st991591950/dhruvparthtapasvi/specialist/Specialist.kt
+++ app/src/main/java/project/st991591950/dhruvparthtapasvi/specialist/Specialist.kt
@@ -1,3 +1,3 @@
 package project.st991591950.dhruvparthtapasvi.specialist
 
-class SpecialistList (val specialistName: String, val speciality: String, val clinicName: String)
+class SpecialistList (val imageResource: Int, val specialistName: String, val speciality: String, val clinicName: String)
