a2e3309e22a0ea926cf8052cb02cdf0085d5de88
parthppc <contracp@sheridancollege.ca>
2022-12-01 21:35:38 -0500

    code intendation

--- app/src/main/java/project/st991591950/dhruvparthtapasvi/login/LoginFragment.kt
+++ app/src/main/java/project/st991591950/dhruvparthtapasvi/login/LoginFragment.kt
@@ -27,8 +27,6 @@ class LoginFragment : Fragment() {
         const val SIGN_IN_RESULT_CODE = 1001
     }
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -48,26 +46,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        binding.CustLogin.setOnClickListener {
-//            findNavController().navigate(R.id.action_SecondFragment_to_customerLogin)
-//        }
-//
-//        binding.ProfLogin.setOnClickListener{
-//            findNavController().navigate(R.id.action_SecondFragment_to_professionalLogin)
-//        }
-        observeAuthenticationState()
-//        binding.CustLogin.setOnClickListener{
-//            AuthState = 1
-//            launchSignInFlow()
-//        }
-//        binding.ProfLogin.setOnClickListener{
-//            AuthState = 2
-//            launchSignInFlow()
-//        }
-//        binding.authButton.setOnClickListener{
-//            launchSignInFlow()
-//        }
+        NavigateAuthenticationStateFragment()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -75,41 +54,29 @@ class LoginFragment : Fragment() {
         if (requestCode == SIGN_IN_RESULT_CODE) {
             val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
-                // User successfully signed in
                 Log.i(
                     TAG,
                     "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
                 )
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+              Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
             }
         }
     }
 
-    private fun observeAuthenticationState() {
-        val factToDisplay = viewModel.getFactToDisplay(requireContext())
+    private fun NavigateAuthenticationStateFragment() {
 
         viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
             when (authenticationState) {
                 LoginViewModel.AuthenticationState.AUTHENTICATED -> {
-
                     if (fraagment == 1){
-
                         findNavController().navigate(R.id.action_loginFragment_to_adminFragment)
-
                     }
                     else{
-                       // findNavController().navigate(R.id.action_loginFragment_to_adminFragment)
                         findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-
                     }
-
                 }
                 else -> {
-
 
                     binding.authButton.setOnClickListener {
                         fraagment = 0
@@ -125,17 +92,10 @@ class LoginFragment : Fragment() {
     }
 
     private fun launchSignInFlow() {
-        // Give users the option to sign in / register with their email
-        // If users choose to register with their email,
-        // they will need to create a password as well
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build()
-            //
-        )
 
-        // Create and launch sign-in intent.
-        // We listen to the response of this activity with the
-        // SIGN_IN_RESULT_CODE code
+        )
         startActivityForResult(
             AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
                 providers
