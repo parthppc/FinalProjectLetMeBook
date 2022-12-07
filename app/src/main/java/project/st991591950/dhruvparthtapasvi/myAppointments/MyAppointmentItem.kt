a2fa3ead9ae5970d71dc50732ff1ccf163d907f0
Dhruvshukla2000 <65990748+Dhruvshukla2000@users.noreply.github.com>
2022-12-07 13:19:13 -0500

    Adjusted MyAppointment Data class and the MyAppointmentsRecycleView.kt as per the firestore database, to fetch and display the booked appointments of the user.

--- app/src/main/java/project/st991591950/dhruvparthtapasvi/myAppointments/MyAppointmentItem.kt
+++ app/src/main/java/project/st991591950/dhruvparthtapasvi/myAppointments/MyAppointmentItem.kt
@@ -3,4 +3,4 @@ package project.st991591950.dhruvparthtapasvi.myAppointments
 import com.google.firebase.Timestamp
 import com.google.type.DateTime
 
-class MyAppointmentList (val sName: String? = null, val reason: String? = null,val title: String? = null, val dateTime:  Timestamp? = null)
+class MyAppointmentList (val appointmentDate: String? = null, val appointmentTime: String? = null, val reason: String? = null, val sName: String? = null,)
