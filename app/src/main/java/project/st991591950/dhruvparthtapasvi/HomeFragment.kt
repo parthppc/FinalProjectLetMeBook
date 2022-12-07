eb9a7f5983e059747a393c7862d6fff0ac7ff58e
Tapasvi Patel <patapasv@sheridancollege.ca>
2022-12-07 01:06:49 -0500

    UI Changes made in Home Page.

--- app/src/main/java/project/st991591950/dhruvparthtapasvi/HomeFragment.kt
+++ app/src/main/java/project/st991591950/dhruvparthtapasvi/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import project.st991591950.dhruvparthtapasvi.databinding.FragmentHomeBinding
@@ -15,9 +17,7 @@ import project.st991591950.dhruvparthtapasvi.databinding.FragmentHomeBinding
  */
 class HomeFragment : Fragment() {
 
-
     private var _binding: FragmentHomeBinding? = null
-
 
     //private val specialistbutton: Button = button_Specialists
     // This property is only valid between onCreateView and
@@ -34,15 +34,14 @@ class HomeFragment : Fragment() {
 //
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
          val root: View = binding.root
-//
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+
+        setActivityTitle(R.string.home_fragment_label)
+        //setActivityTitle("Home")
         return root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setActivityTitle(R.string.home_fragment_label)
 
         binding.btnLogout.setOnClickListener{
             FirebaseAuth.getInstance().signOut();
@@ -70,5 +69,14 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+    //code to apply title name from the string
+    private fun Fragment.setActivityTitle(@StringRes id: Int) {
+        (activity as? AppCompatActivity?)?.supportActionBar?.title = getString(id)
+    }
+
+    //code to directly apply the title name in the fragment itself.
+    fun Fragment.setActivityTitle(title: String) {
+        (activity as? AppCompatActivity?)?.supportActionBar?.title = title
+    }
 
 }
